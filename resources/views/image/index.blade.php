a72bc5fc0bd10fedcdb8fc2e8c906c4f27c403a2
asperezm <47333056+asperezm@users.noreply.github.com>
2020-11-14 18:46:45 -0500

    id

--- resources/views/image/index.blade.php
+++ resources/views/image/index.blade.php
@@ -5,6 +5,7 @@
         <body>
             <h1> LA IMAGEN RANDOM ES ........ </h1>
             <img src="{{ $data['storage']->url($data['imageName']) }}" alt="">
+            <h4> Powered by: {{ $data['id'] }} </h4>
         </body>
     </div>
 </div>
